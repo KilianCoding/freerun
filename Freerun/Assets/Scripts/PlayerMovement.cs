5b2996601e61bc8b8112e1914d265718e1fc57b2
femboycoding <kilianmarzella@outlook.com>
2021-12-06 18:13:52 +0000

    added functional groundcheck and started flight

--- Freerun/Assets/Scripts/PlayerMovement.cs
+++ Freerun/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,9 @@ public class PlayerMovement : MonoBehaviour
     public float jump = 10f;
     public float flight = 1f;
     Vector3 velocity;
+    public Transform groundCheck;
+    float groundDistance = 0.2f;
+    public LayerMask groundMask; //What layer the object is on
     // Start is called before the first frame update
     void Start()
     {
@@ -36,27 +39,26 @@ public class PlayerMovement : MonoBehaviour
         {
             speed = walkspeed;
         }
-
-        if (Input.GetButtonDown("Jump") && isGrounded()) //If on ground and jump //ButtonDown only activated the frame that button is pressd
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //If touching ground, returns true
+        if (isGrounded)
         {
-            velocity.y = jump;
+            flight = 1;
+            if (Input.GetButtonDown("Jump")) //If on ground and jump //ButtonDown only activated the frame that button is pressd
+            {
+                velocity.y = jump;
+            }
         }
-        else if (Input.GetButton("Jump") && !isGrounded() && flight > 0) //If space held while mid air  //GetButton activated every frame while space is pressed
+        /*if (Input.GetButton("Jump") && !isGrounded && flight > 0) //If space held while mid air  //GetButton activated every frame while space is pressed
         {
-            flight -= 1f * Time.deltaTime; //Every frame you are flying, you can fly less
-            velocity.y = 10f; 
-        }
+            flight -= 5f * Time.deltaTime; //Every frame you are flying, you can fly less
+            velocity.y = 5f; 
+        }*/
 
         Vector3 move = transform.right * x + transform.forward * z; // Increase move right by x amount and move forwards by z amount. If s is pressed, then z would be negtive, moving you backwards
         CC.Move(move * speed * Time.deltaTime); //Apply move to the Character controller, * speed (to determine the speed) and Time.deltaTime
-        //Time.deltaTime is the time elapsed between frames. EG: At 20 fps, it is 0.05 and at 10fps it is 0.1. This means that movement is consistent no matter what your fps
+        //Time.deltaTime is the time elapsed between frames. EG: At 20 fps, it is 0.05 and at ps it is 0.1. This means that movement is consistent no matter what your fps
 
         velocity.y += gravity * Time.deltaTime; //Adds gravity
         CC.Move(velocity * Time.deltaTime); //Applies gravity
-    }
-    private bool isGrounded()
-    {
-        flight = 10f; //Once you land, you can fly again
-        return false;
     }
 }
